ba543997cc49cb71eb6ba3946c542c03a60064c8
marucg <marucarrillo08@gmail.com>
2019-02-06 13:26:28 +0100

    Count of As and entering a null string

---
+++ session-5/ex-1-user.py
@@ -0,0 +1,34 @@
+#when i press enter without typing any sequence
+def count_a(seq):
+    '''Counting the numbers of As in the sequence'''
+    #that is for comenting. it is necessary
+
+
+    #Counter of As
+    result = 0
+    for b in seq:
+        if b == 'A':
+            result += 1
+
+    #return the result
+    return result
+
+#Main program
+s = input('Please enter the sequence: ')
+number_a = count_a(s)
+print('The number of As is: ',number_a)
+
+#Calculating the length of sequence
+total_length = len(s)
+
+#Calculate the percentage of As in the sequence
+if total_length > 0:
+    percentage = round(100.0 * number_a / total_length, 1)
+else:
+    percentage = 0
+
+print('The total length is: ', total_length)
+print('The percentage of As is: ', percentage)
+
+#ALWAYS bug, it will lead me to the error
+#For functions click the second botton
