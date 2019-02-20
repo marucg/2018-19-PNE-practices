5ae5ee174b3409a3eb612bf74b0b420f21c8cdfe
marucg <marucarrillo08@gmail.com>
2019-02-20 22:11:27 +0100

    client introducing a message

--- session 9/client_ex-1.py
+++ session 9/client_ex-1.py
@@ -1,13 +1,16 @@
 import socket
+from termcolor import colored
 
 # SERVER IP, PORT
-IP = "212.128.253.96"
-PORT = 8082
+IP = "10.0.2.15"
+PORT = 8087
 
 # WAIT UNTIL I HAVE ALL THE DATA LIKE AN INPUT TO CONNECT TO THE SERVER
-while True:
+option = True
+while option:
     # The client is blocking the server....  NOT A GOOD DESIGN!!!
     #--------PUT THE INPUT FIRST--------
+
     msg = input("Please enter a message (to exit write 'EXIT'): ")
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,8 +24,20 @@ while True:
     # Receive the servers respoinse
     response = s.recv(2048).decode()
 
-    s.close()
-
     # Print the server's response
-    print("Response: {}".format(response))
+    print("Response: ", response)
+
+    final_message = colored('-PROGRAM FINISHED-', 'red')
+
+    if response == final_message:
+        s.close()
+        break
+
+    else:
+        s.close()
+
+
+
+
+
 
