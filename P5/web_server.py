b640424a5a0bb6cd0a8664be37faa636a70f2d36
marucg <marucarrillo08@gmail.com>
2019-03-06 18:31:40 +0100

    Web server using the http module

---
+++ P5/web_server.py
@@ -0,0 +1,40 @@
+import http.server
+import socketserver
+
+PORT = 8006
+
+
+# CREATING AND OBJECT  -HANDLER-
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('Get received')
+        print('Request Line:' + self.requestline)
+        print(' cmd: ' + self.command)
+        print(' path: ' + self.path)
+
+        if self.path == '/':
+            file = 'index.html'
+        elif self.path == '/pink':
+            file = 'pink.html'
+        elif self.path == '/blue':
+            file = 'blue.html'
+        else:
+            file = 'error.html'
+
+        with open(file, 'r') as r:
+            content = r.read()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-length', len(str.encode(content)))
+        self.end_headers()
+        self.wfile.write(str.encode(content))
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print('Serving at port', PORT)
+
+    httpd.serve_forever()
